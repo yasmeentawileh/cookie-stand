52a1c6d7f6ad0909a68f0f6675c15fa3e7af5b2e
yasmeentawileh <yasmeen.tawileh@gmail.com>
2020-09-13 22:31:21 +0300

    class06

--- js/app.js
+++ js/app.js
@@ -8,22 +8,25 @@ var Seattle = {
     maxcustomer: 65,
     averagecookiesale: 6.3,
     customerperhour: [],
-    cookiesperhour: [],
+    cookiesperhour:[],
     hoursofworking: ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'],
 
     getCustomerperhour: function () {
-        this.customerperhour.push(getRandomNumber(this.mincustomer, this.maxcustomer));
+        this.customerperhour.push((this.mincustomer, this.maxcustomer));
         console.log(this.customerperhour + ' number of customers per hour');
+
     },
 
     thecookiesPerhour: function () {
         this.getCustomerperhour();
         for (var i = 0; i <= this.hoursofworking.length; i++) {
+
             var cookie = this.averagecookiesale * this.customerperhour[i];
             // this.cookiesPerhour.push(cookie);
             // console.log(this.cookiesperhour + ' number of sold cookies per hour');
-            console.log(cookie);
-            console.log(this.customerperhour[i]);
+
+            console.log(cookie +' number of sold cookies per hour');
+            console.log(this.cookiesperhours);
         }
     }
 
@@ -41,17 +44,9 @@ function getRandomNumber(mincustomer, maxcustomer) {
         var random = Math.random();
         random = (random * (maxcustomer - mincustomer + 1)) + mincustomer;
         random = Math.floor(random);
-        // console.log('yasmeen');
         // console.log(random);
         return random;
-
 }
 
-// to make the number of cookies that paid ber hour
-// seattle.getCookiesperhour();
-// function getCookiesarray() {
-//     for (var i = 0; i < 14; i++) {
-//         cookiesperhour = customerperhour * averagecookiesale;
-//         var arr = cookiesperhour];
-//         resturn arr;
-//     }
+
+
